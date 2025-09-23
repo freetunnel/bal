12b9374f2140bc98c8d5da542cc01e00ab959d62
freetunnel <153220342+freetunnel@users.noreply.github.com>
2025-09-23 14:54:18 +0700

    Update ssh-vpn.sh

--- system/ssh-vpn.sh
+++ system/ssh-vpn.sh
@@ -174,8 +174,12 @@ systemctl start badvpn3
 # setting port ssh
 cd
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sed -i '/Port 22/a Port 500' /etc/ssh/sshd_config
+sed -i '/Port 22/a Port 40000' /etc/ssh/sshd_config
+sed -i '/Port 22/a Port 51443' /etc/ssh/sshd_config
+sed -i '/Port 22/a Port 58080' /etc/ssh/sshd_config
 sed -i '/Port 22/a Port 200' /etc/ssh/sshd_config
-
+sed -i '/Port 22/a Port 22' /etc/ssh/sshd_config
 /etc/init.d/ssh restart
 
 echo "=== Install Dropbear ==="
