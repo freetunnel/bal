224c970713138a289a10765e346facdad190e25b
freetunnel <153220342+freetunnel@users.noreply.github.com>
2025-09-24 23:05:48 +0700

    Update insshws.sh

--- websocket/insshws.sh
+++ websocket/insshws.sh
@@ -20,7 +20,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/ws-dropbear
+ExecStart=/usr/bin/python3 -O /usr/local/bin/ws-dropbear
 Restart=on-failure
 
 [Install]
@@ -75,7 +75,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/ws-stunnel
+ExecStart=/usr/bin/python3 -O /usr/local/bin/ws-stunnel
 Restart=on-failure
 
 [Install]
